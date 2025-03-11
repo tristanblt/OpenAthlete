c05ca92e85f0b4f99ac4cb594be089384dfe0601
Tristan Bouillot <tristanblt@gmail.com>
2025-03-11 22:45:51 +0100

    feat(web): add node polyfills plugin and update dependencies in package.json

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,10 +2,11 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), nodePolyfills()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
