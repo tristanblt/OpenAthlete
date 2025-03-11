23e110a9194ec598740c7c43744a06f31f9cff88
Tristan Bouillot <tristanblt@gmail.com>
2025-03-11 22:53:35 +0100

    feat(web): integrate React Query and Auth context in App component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,9 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 
+import { AuthConsumer, AuthProvider } from './contexts/auth';
 import router from './routes/sections';
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthConsumer>
+          <RouterProvider router={router} />
+        </AuthConsumer>
+      </QueryClientProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
