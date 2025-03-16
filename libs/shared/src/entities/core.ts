b4b8c7da74264a65f9202bdedf5b50394a658016
Tristan Bouillot <tristanblt@gmail.com>
2025-03-16 11:51:53 +0100

    feat(shared): add event interfaces and type for event management

--- libs/shared/src/entities/core.ts
+++ libs/shared/src/entities/core.ts
@@ -1,4 +1,4 @@
-import { athlete, user, user_role } from '@openathlete/database';
+import { athlete, event_type, user, user_role } from '@openathlete/database';
 
 import { ConvertKeysToCamelCase } from '../utils/data.mapper';
 
@@ -12,3 +12,20 @@ export interface User extends ConvertKeysToCamelCase<user> {
 export interface Athlete extends ConvertKeysToCamelCase<athlete> {
   user?: User;
 }
+
+export type EventType = ConvertKeysToCamelCase<event_type>;
+
+export interface Event {
+  event_id: number;
+  name: string;
+  athlete_id: number;
+  start_date: Date;
+  end_date: Date;
+  type: EventType;
+}
+
+export interface TrainingEvent extends Event {}
+
+export interface CompetitionEvent extends Event {}
+
+export interface NoteEvent extends Event {}
