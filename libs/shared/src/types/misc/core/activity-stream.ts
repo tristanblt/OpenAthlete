6f86b75c511b1638f546840ccbfb30aa086cb3fd
Tristan Bouillot <tristanblt@gmail.com>
2025-03-29 12:46:38 +0100

    feat(shared): add ActivityStream interface for activity data representation

---
+++ libs/shared/src/types/misc/core/activity-stream.ts
@@ -0,0 +1,10 @@
+export interface ActivityStream {
+  time?: number[];
+  distance?: number[];
+  latlng?: number[][];
+  altitude?: number[];
+  heartrate?: number[];
+  cadence?: number[];
+  watts?: number[];
+  temp?: number[];
+}
--- libs/shared/src/types/misc/core/index.ts
+++ libs/shared/src/types/misc/core/index.ts
@@ -1,3 +1,4 @@
 export * from './event-type.enum';
 export * from './sport-type.enum';
 export * from './training-zone-type.enum';
+export * from './activity-stream';
