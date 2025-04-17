7ce0513fbe6aa6d98b2661f94830f2f3d36a0ca8
Tristan Bouillot <tristanblt@gmail.com>
2025-04-17 16:30:24 +0200

    refactor(app): clean up unused dropdown menu items in NavUser component

--- apps/web/src/components/sidebar/nav-user.tsx
+++ apps/web/src/components/sidebar/nav-user.tsx
@@ -75,33 +75,14 @@ export function NavUser() {
                 </div>
               </div>
             </DropdownMenuLabel>
-            {/* <DropdownMenuSeparator /> */}
-            {/* <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup> */}
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              {/* <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem> */}
               <DropdownMenuItem
                 onClick={() => navigate(getPath(['dashboard', 'settings']))}
               >
                 <CogIcon />
                 Settings
               </DropdownMenuItem>
-              {/* <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Bell />
-                Notifications
-              </DropdownMenuItem> */}
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem onClick={() => logout()}>
