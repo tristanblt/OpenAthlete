bca332216b6e583796e6ef49f84465bf243e4a00
Tristan Bouillot <tristanblt@gmail.com>
2025-04-18 15:25:49 +0200

    fix(app): remove console.log statement from TrainingCompetitionDetails component

--- apps/web/src/components/event-details/training-competition-details.tsx
+++ apps/web/src/components/event-details/training-competition-details.tsx
@@ -23,7 +23,6 @@ export function TrainingCompetitionDetails({ event }: P) {
   const setRelatedActivityMutation = useSetRelatedActivityMutation();
   const unsetRelatedActivityMutation = useUnsetRelatedActivityMutation();
 
-  console.log(event);
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       <Card className="col-span-1">
