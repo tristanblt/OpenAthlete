627d86727744bbd64a3b02ae812fe1795dd4986e
Tristan Bouillot <tristanblt@gmail.com>
2025-04-20 18:18:16 +0200

    fix(app): improve getWeekPeriod calculation for accurate week start and end dates

--- libs/shared/src/utils/date.ts
+++ libs/shared/src/utils/date.ts
@@ -15,12 +15,15 @@ export const getMonthPeriod = (date: Date): { start: Date; end: Date } => {
   const end = new Date(date.getFullYear(), date.getMonth() + 1, 0);
   return { start: startOfDay(start), end: endOfDay(end) };
 };
-
 export const getWeekPeriod = (date: Date): { start: Date; end: Date } => {
   const start = new Date(date);
-  const end = new Date(date);
-  start.setDate(start.getDate() - start.getDay());
-  end.setDate(end.getDate() + (6 - end.getDay()));
+  const day = start.getDay();
+  const diff = (day < 1 ? -6 : 1) - day;
+  start.setDate(start.getDate() + diff);
+  start.setHours(0, 0, 0, 0);
+  const end = new Date(start);
+  end.setDate(end.getDate() + 6);
+  end.setHours(23, 59, 59, 999);
   return { start: startOfDay(start), end: endOfDay(end) };
 };
 
