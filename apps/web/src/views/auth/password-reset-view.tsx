2db3405e6be6dcf07d38e7effc9e7f70bcd821c2
Tristan Bouillot <tristanblt@gmail.com>
2025-04-22 12:49:59 +0200

    feat(app): implement password reset view with form handling and success/error notifications

--- apps/web/src/views/auth/password-reset-view.tsx
+++ apps/web/src/views/auth/password-reset-view.tsx
@@ -1,35 +1,37 @@
-import { StravaIcon } from '@/assets/icons';
 import { FormProvider, RHFTextField } from '@/components/hook-form';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
-import { useAuthContext } from '@/contexts/auth';
 import { getPath } from '@/routes/paths';
-import { useLoginMutation } from '@/services/auth';
+import { usePasswordResetMutation } from '@/services/user';
 import { cn } from '@/utils/shadcn';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 import z from 'zod';
 
-import { loginDtoSchema } from '@openathlete/shared';
+import { passwordResetSchema } from '@openathlete/shared';
 
 export function PasswordResetView({ className }: React.ComponentProps<'form'>) {
-  const { initialize } = useAuthContext();
-  const navigate = useNavigate();
-  const loginMutation = useLoginMutation({
+  const nav = useNavigate();
+  const passwordResetMutation = usePasswordResetMutation({
     onSuccess: async () => {
-      await initialize();
-      navigate(getPath(['dashboard']));
+      nav(getPath(['auth', 'login']));
+      toast.success('Password updated successfully');
+    },
+    onError: () => {
+      toast.error('Error updating password');
     },
   });
-  const methods = useForm<z.infer<typeof loginDtoSchema>>({
-    resolver: zodResolver(loginDtoSchema),
-    defaultValues: { email: '', password: '' },
+  const methods = useForm<z.infer<typeof passwordResetSchema>>({
+    resolver: zodResolver(passwordResetSchema),
+    defaultValues: { token: '', password: '' },
   });
 
   const { handleSubmit } = methods;
 
-  const onSubmit = handleSubmit(async (data) => loginMutation.mutate(data));
+  const onSubmit = handleSubmit(async (data) =>
+    passwordResetMutation.mutate(data),
+  );
 
   return (
     <FormProvider
@@ -38,59 +40,29 @@ export function PasswordResetView({ className }: React.ComponentProps<'form'>) {
       className={cn('flex flex-col gap-6', className)}
     >
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
+        <h1 className="text-2xl font-bold">Reset your password</h1>
         <p className="text-muted-foreground text-sm text-balance">
-          Enter your email below to login to your account
+          Enter your new password and we will update it for you.
         </p>
       </div>
       <div className="grid gap-6">
         <div className="grid gap-3">
           <RHFTextField
-            name="email"
-            type="email"
-            placeholder="m@example.com"
-            label="Email"
+            name="password"
+            type="password"
+            placeholder="********"
+            label="New password"
             required
           />
-        </div>
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-            <Link
-              to="/auth/forgot-password"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </Link>
-          </div>
-          <RHFTextField name="password" type="password" required />
         </div>
         <Button
           type="submit"
           className="w-full"
           onClick={onSubmit}
-          isLoading={loginMutation.isPending}
+          isLoading={passwordResetMutation.isPending}
         >
-          Login
+          Update password
         </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or continue with
-          </span>
-        </div>
-        <Button variant="outline" className="w-full">
-          <StravaIcon />
-          Login with Strava
-        </Button>
-      </div>
-      <div className="text-center text-sm">
-        Don&apos;t have an account?{' '}
-        <Link
-          to="/auth/create-account"
-          className="underline underline-offset-4"
-        >
-          Sign up
-        </Link>
       </div>
     </FormProvider>
   );
