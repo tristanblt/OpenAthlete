e4ac42592fc410b588782d3e0899f7e82791e9e1
Tristan Bouillot <tristanblt@gmail.com>
2025-04-22 13:11:52 +0200

    refactor(api): replace SendGrid import with sgMail for email sending

--- apps/api/src/modules/notification/services/notification.service.ts
+++ apps/api/src/modules/notification/services/notification.service.ts
@@ -1,5 +1,3 @@
-import { MailDataRequired, default as SendGrid } from '@sendgrid/mail';
-
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
@@ -7,22 +5,24 @@ import { ApiEnvSchemaType, EmailId, emailLibrary } from '@openathlete/shared';
 
 import { SendEmail } from '../types';
 
+const sgMail = require('@sendgrid/mail');
+
 @Injectable()
 export class NotificationService {
   constructor(private configService: ConfigService<ApiEnvSchemaType, true>) {
-    SendGrid.setApiKey(configService.get('SENDGRID_API_KEY') ?? '');
+    sgMail.setApiKey(configService.get('SENDGRID_API_KEY') ?? '');
   }
 
   async sendEmail<T extends EmailId>(payload: SendEmail<T>) {
     try {
-      await SendGrid.send({
+      await sgMail.send({
         to: payload.to,
         from: this.configService.get('SENDGRID_FROM_EMAIL'),
         subject: payload.subject || emailLibrary[payload.type].defaultSubject,
         html: `<ul>${Object.entries(payload.params)
           .map(([key, value]) => `<li>${key}: ${value}</li>`)
           .join('')}</ul>`,
-      } as MailDataRequired & { templateId?: string });
+      });
     } catch (error: any) {
       console.error('Error sending email', error?.response?.body);
     }
