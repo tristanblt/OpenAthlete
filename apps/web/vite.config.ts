2cc3679e18d75144f815e9e7be161fa6cf1335bc
Tristan Bouillot <tristanblt@gmail.com>
2025-05-05 19:46:37 +0200

    feat(app): remove machine translation from build script and add paraglide plugin to Vite configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,3 +1,4 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
@@ -6,7 +7,15 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), nodePolyfills()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    nodePolyfills(),
+    paraglideVitePlugin({
+      project: './project.inlang',
+      outdir: './src/paraglide',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
